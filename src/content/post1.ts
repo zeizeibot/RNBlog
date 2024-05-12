672ad8e1e73bde78a73ed0d9ad52abdfe03807f5
Joel Juva <joel_juva@hotmail.com>
2024-05-12 21:42:08 +0300

    Blogi pohjaa

---
+++ src/content/post1.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'post1',
+  date: '2024-02-28T13:27:28Z',
+  title: 'Mun eka julkasu',
+  description: 'Tää on mun eka julkasu',
+  thumbnail: '',
+  content: `
+ 
+  `,
+};
+
+export default post;
