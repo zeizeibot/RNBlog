e3ccf94717d237f0dbce8ae144fd7d244a791831
Joel Juva <joel_juva@hotmail.com>
2024-06-30 19:58:59 +0300

    static site generation

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -1,8 +1,15 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { getPost } from '../repository/postRepository';
+import { getPost, getAllPosts } from '../repository/postRepository';
 import Markdown from 'react-native-markdown-display';
+
+export async function generateStaticParams(): Promise<Record<string, string>[]> {
+    const posts = getAllPosts();
+    // Return an array of params to generate static HTML files for.
+    // Each entry in the array will be a new page.
+    return posts.map(post => ({ slug: post.slug }));
+  }
 
 const PostDetailsPage = () => {
     const { slug } = useLocalSearchParams();
