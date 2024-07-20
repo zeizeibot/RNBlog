7a17db59df8f42634521cb8c83bf92e05a74b53b
Joel Juva <joel_juva@hotmail.com>
2024-07-20 22:13:15 +0300

    slug test

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Platform  } from 'react-native';
 import React, { useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import { getPost, getAllPosts } from '../repository/postRepository';
@@ -13,45 +13,35 @@ export async function generateStaticParams(): Promise<Record<string, string>[]> 
     return posts.map(post => ({ slug: post.slug }));
   }
 
-const PostDetailsPage = () => {
+  const PostDetailsPage = () => {
     const { slug } = useLocalSearchParams();
     const [post, setPost] = useState(getPost(slug));
-    
-if (!post) {
-    return <Text>Sisältöä ei löytynyt!</Text>
-}
-    return(
-        <>
-    <Head>
-        <title>{post.title}</title>
-        <meta name="description" content={post.description} />
-        <meta property="og:image" content={`${ORIGIN}/../dist/thumbnails/${post.thumbnail}`}
+  
+    if (!post) {
+      return <Text>Sisältöä ei löytynyt</Text>;
+    }
+  
+    const imageUrl = Platform.OS === 'web' ? `/dist/thumbnails/${post.thumbnail}` : `${ORIGIN}/dist/thumbnails/${post.thumbnail}`;
+  
+    return (
+      <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
+        {/* Meta tags are only relevant for web */}
+        {Platform.OS === 'web' && (
+          <Head>
+            <title>{post.title}</title>
+            <meta name="description" content={post.description} />
+            <meta property="og:image" content={`${ORIGIN}/dist/thumbnails/${post.thumbnail}`} />
+          </Head>
+        )}
+        <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+        <Image
+          source={{ uri: imageUrl }}
+          style={{ width: '50%', aspectRatio: 16 / 9 }}
+          alt={post.title}
         />
-      </Head>
-        <ScrollView style={{
-            flex: 1,
-            backgroundColor: 'white',
-
-        }}
-        contentContainerStyle={{
-            maxWidth: 960,
-            width: '100%',
-            marginHorizontal: 'auto',
-            padding: 20,
-        }}
-        >
-            <Text style={{ fontSize: 30, marginBottom: 20  }}>{post.title}</Text>
-
-            <Image
-            
-            source={{ uri: `${ORIGIN}/../dist/thumbnails/${post.thumbnail}`}} 
-            style={{ width: '50%', aspectRatio: 16 / 9 }}
-            alt={post.title}
-            />
-            <Markdown>{post.content}</Markdown>
-        </ScrollView>
-        </>
+        <Markdown>{post.content}</Markdown>
+      </ScrollView>
     );
-};
-
-export default PostDetailsPage;
+  };
+  
+  export default PostDetailsPage;
